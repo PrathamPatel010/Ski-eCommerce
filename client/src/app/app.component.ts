27b36697f07eac78b2d3f22fc2769de0fe0bca49
Pratham Patel <ppratham812@gmail.com>
2024-12-29 00:32:50 +0530

    connected client to api and retrieve results

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/Product';
+import { Pagination } from './shared/models/Pagination';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:5000/api/';
+  private http = inject(HttpClient);
+  products: Product[] = [];
   title = 'SkiNet';
+  ngOnInit() {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: (res) => {
+        console.log(res.items);
+        this.products = res.items;
+      },
+      error: (err) => console.log(err),
+      complete: () => console.log('complete'),
+    });
+  }
 }
---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,10 @@
+export type Product = {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  pictureURL: string;
+  type: string;
+  brand: string;
+  quantityInStock: number;
+};
